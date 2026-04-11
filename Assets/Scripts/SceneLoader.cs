b52b38c2d87bfdfeafdbd2697bd97cf89440f49e
Fire Cat705 <134724491+FireCat705@users.noreply.github.com>
2026-04-10 19:01:51 -0700

    Main Menu should Work AAAA

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void LevelScene()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
