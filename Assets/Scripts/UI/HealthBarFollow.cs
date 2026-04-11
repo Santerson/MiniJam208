22083b89283404bcac122a5181d39ea223955172
Fire Cat705 <134724491+FireCat705@users.noreply.github.com>
2026-04-10 22:20:48 -0700

    idk

---
+++ Assets/Scripts/UI/HealthBarFollow.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class HealthBarFollow : MonoBehaviour
+{
+    Transform Player;
+    [SerializeField] Vector3 offset = new Vector3(0, -50, 0);
+
+    private void LateUpdate()
+    {
+        
+    }
+}
