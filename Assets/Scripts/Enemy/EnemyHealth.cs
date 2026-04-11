1d2a23d2fcd6a72a0fcb449641a2438c3d2eb577
Fire Cat705 <134724491+FireCat705@users.noreply.github.com>
2026-04-10 18:09:19 -0700

    Special Enemies
    Special Enemies now drop different souls

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -3,17 +3,40 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     PlayerAttack PA;
+
+    [Header("SOULS")]
+    [SerializeField] GameObject soulBasicPrefab;
+    [SerializeField] GameObject soulDMGPrefab;
+    [SerializeField] GameObject soulAttackRangePrefab;
+    [SerializeField] GameObject soulASPPrefab;
+
+    [Header("Test")]
     [SerializeField]float health = 5f;
     private void Update()
     {
         if (health <= 0 && gameObject.CompareTag("BiggerBadderEnemy").Equals(false))
         {
-            Destroy(this);
+            Destroy(gameObject);
         }
         else if (health <= 0 && gameObject.CompareTag("BiggerBadderEnemy").Equals(true))
         {
+            switch (Random.Range(1, 5))
+            {
+                case 1: 
+                    Instantiate(soulBasicPrefab, transform.position, Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(soulDMGPrefab, transform.position, Quaternion.identity);
+                    break;
+                case 3:
+                    Instantiate(soulAttackRangePrefab, transform.position, Quaternion.identity);
+                    break;
+                case 4:
+                    Instantiate(soulASPPrefab, transform.position, Quaternion.identity);
+                    break;
+            }
             Debug.Log("Spawwn Soul");
-            Destroy(this);
+            Destroy(gameObject);
         }
     }
 
@@ -21,16 +44,16 @@ public class EnemyHealth : MonoBehaviour
     {
         if (AttackTrigger.gameObject.CompareTag("PlayerAttack"))
         {
-            PA = AttackTrigger.gameObject.GetComponent<PlayerAttack>();
+            PA = AttackTrigger.gameObject.GetComponentInParent<PlayerAttack>();
             if (PA.IsAttacking)
             {
-                DamageTaken();
+                DamageTaken(PA.currentAttackDamage);
             }
         }
     }
 
-    private void DamageTaken()
+    private void DamageTaken(float damage)
     {
-        health -= PA.currentAttackDamage;
+        health -= damage;
     }
 }
