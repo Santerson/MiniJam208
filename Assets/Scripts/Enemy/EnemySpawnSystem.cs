8a2b9fb2a38ad065dd2d5931ba5509cacc405280
Fire Cat705 <134724491+FireCat705@users.noreply.github.com>
2026-04-11 16:16:56 -0700

    Wave System Done

--- Assets/Scripts/Enemy/EnemySpawnSystem.cs
+++ Assets/Scripts/Enemy/EnemySpawnSystem.cs
@@ -8,29 +8,60 @@ public class EnemySpawnSystem : MonoBehaviour
     Transform player; // to make sure its not near player
     float minRadius = 15f; // the min of spawning distance
     float maxRadius = 25f; // the maximum of spawning distance
-    [SerializeField] float maxSpawnEnemiesPerWave = 50f;
+
+    [Tooltip ("FIX THESE FUCKING NAMES (BISHEP)")]
+    [Header("Wave/Spawning")]
+    [SerializeField] float StartSpawnedEnemies = 8f;
+    [SerializeField] float spawnedStartEnemies = 0f;
+    [SerializeField] float maxSpawnEnemiesPerWave = 15f;
     [SerializeField] float spawnedEnemiesInWave = 0f;
-    float WaveTimer = 30f;
+    float IntervalBetweenWaves = 60f;
+    float WaveTimer = 5;
     float ResetWaveTimer;
+    float ResetIntervalBetweenWaves;
+    float RegualSpawnTime = 2f;
+    float ResetSpawnTime;
+    [SerializeField] float RegualMaxSpawn = 50f;
+    public float RegularEnemiesSpawn = 0;
+    bool spawnedStartEnemiesBool = false;
 
     private void Awake()
     {
         ResetWaveTimer = WaveTimer;
-        WaveTimer = 5f;
+        ResetIntervalBetweenWaves = IntervalBetweenWaves;
+        ResetSpawnTime = RegualSpawnTime;
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
 
     private void Update()
     {
-        WaveTimer -= Time.deltaTime;
-        if (WaveTimer <= 0)
+        if (spawnedStartEnemiesBool == false)
         {
             SpawnEnemy();
-            if (spawnedEnemiesInWave == maxSpawnEnemiesPerWave)
+        }
+        else if (IntervalBetweenWaves <= 0f) // for after the Inbetween waves
+        {
+            WaveTimer -= Time.deltaTime; // Wave Countdown
+            if (WaveTimer <= 0f)
             {
-                WaveTimer = ResetWaveTimer;
+                SpawnEnemy();
             }
+        }
+        else
+        {
+            if (RegualSpawnTime <= 0 && RegularEnemiesSpawn != RegualMaxSpawn) // Regular Version
+            {
+                SpawnEnemy();
+                RegularEnemiesSpawn++;
+                RegualSpawnTime = ResetSpawnTime;
+                IntervalBetweenWaves -= Time.deltaTime;
+            }
+            RegualSpawnTime -= Time.deltaTime;
+        }
+        if (WaveTimer <= 0 && spawnedEnemiesInWave == maxSpawnEnemiesPerWave) // to fix the WaveTimer after its done
+        {
+            WaveTimer = ResetWaveTimer;
         }
     }
 
@@ -51,16 +82,36 @@ public class EnemySpawnSystem : MonoBehaviour
 
         // Spawning Enemy
         GameObject Enemy = Instantiate(Enemys[Random.Range(0, Enemys.Length)], spawnPosition, Quaternion.identity);
-        spawnedEnemiesInWave++;
+
+        if (spawnedStartEnemies < StartSpawnedEnemies) // for the starting spawned enemies
+        {
+            spawnedStartEnemies++;
+        }
+        if (spawnedStartEnemies >= StartSpawnedEnemies) // to do Waves spawning
+        {
+            spawnedStartEnemiesBool = true;
+            if (IntervalBetweenWaves == 0)
+            {
+                if (spawnedEnemiesInWave > maxSpawnEnemiesPerWave)
+                {
+                    spawnedEnemiesInWave++;
+                }
+                if (spawnedEnemiesInWave == maxSpawnEnemiesPerWave)
+                {
+                    spawnedEnemiesInWave = 0;
+                    IntervalBetweenWaves = ResetIntervalBetweenWaves;
+                }
+            }
+        }
 
 
-        if (Random.Range(1, 100) <= 35)
+        if (Random.Range(1, 100) <= 35) // so the can spawn enemies
         {
             Enemy.tag = "BiggerBadderEnemy";
         }
         else
         {
-                Enemy.tag = "Enemy";
+            Enemy.tag = "Enemy";
         }
     }
 }
